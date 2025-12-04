847559b935a84b05a132acbf8ded241cd158a449
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:54:33 +0200

    Update MealService.java

--- src/hotel/MealService.java
+++ src/hotel/MealService.java
@@ -1,7 +1,23 @@
 package hotel;
 
 public class MealService extends Service {
-    public MealService(String name, double price) { super(name, price); }
+
+    public MealService(String name, double price) {
+        super(name, price);
+    }
+
     @Override
-    public void use() { System.out.println("Замовлено харчування: " + name); }
+    public void use() {
+        System.out.println("Замовлено харчування: " + name);
+    }
+
+    @Override
+    public void getServiceInfo() {
+        System.out.println("MealService: " + name + ", Ціна: " + price);
+    }
+
+    @Override
+    public double calculateCost() {
+        return price;
+    }
 }
