cfec30011e9ca0ef9da60fc60222a97e12c477c5
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:53:57 +0200

    Update DataStorage.java

--- src/hotel/DataStorage.java
+++ src/hotel/DataStorage.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataStorage {
-    public static List<Guest> guests = new ArrayList<>();
-    public static List<Room> rooms = new ArrayList<>();
+    public static List<hotel.Guest> guests = new ArrayList<>();
+    public static List<hotel.Room> rooms = new ArrayList<>();
     public static List<Booking> bookings = new ArrayList<>();
 
-    public static void addGuest(Guest g) { guests.add(g); }
-    public static void addRoom(Room r) { rooms.add(r); }
+    public static void addGuest(hotel.Guest g) { guests.add(g); }
+    public static void addRoom(hotel.Room r) { rooms.add(r); }
     public static void addBooking(Booking b) { bookings.add(b); }
 }
