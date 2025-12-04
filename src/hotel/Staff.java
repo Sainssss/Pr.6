8d86883278d6a2449a3f8a1cca8e2ef251234ead
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:55:54 +0200

    Update Staff.java

--- src/hotel/Staff.java
+++ src/hotel/Staff.java
@@ -1,8 +1,32 @@
 package hotel;
 
 public class Staff extends Person {
-    String position;
-    public Staff(String n, String ph, String pos) { super(n, ph); position = pos; }
+    private String position;
+    private String schedule;
+
+    public Staff(String name, String phone, String position, String schedule) {
+        super(name, phone); // Конструктор Person с двумя параметрами
+        this.position = position;
+        this.schedule = schedule;
+    }
+
     @Override
-    public void showInfo() { System.out.println(name + ", " + position + ", Тел: " + phone); }
+    public void showInfo() {
+        System.out.println("Працівник: " + name + ", Посада: " + position + ", Телефон: " + phone + ", Графік: " + schedule);
+    }
+
+    @Override
+    public void checkIn() {
+        System.out.println(name + " почав роботу.");
+    }
+
+    @Override
+    public void checkOut() {
+        System.out.println(name + " закінчив роботу.");
+    }
+
+    public void changeSchedule(String newSchedule) {
+        schedule = newSchedule;
+        System.out.println("Новий графік для " + name + ": " + schedule);
+    }
 }
