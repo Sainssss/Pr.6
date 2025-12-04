6db838674d9f8cafe06c6ec913d2469130b062bf
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:54:09 +0200

    Update Guest.java

--- src/hotel/Guest.java
+++ src/hotel/Guest.java
@@ -1,8 +1,25 @@
 package hotel;
 
 public class Guest extends Person {
-    String email;
-    public Guest(String n, String ph, String em) { super(n, ph); email = em; }
+    private String email;
+
+    public Guest(String name, String phone, String email) {
+        super(name, phone); // вызов конструктора Person
+        this.email = email;
+    }
+
     @Override
-    public void showInfo() { System.out.println(name + ", " + phone + ", " + email); }
+    public void showInfo() {
+        System.out.println("Гість: " + name + ", Телефон: " + phone + ", Email: " + email);
+    }
+
+    @Override
+    public void checkIn() {
+        System.out.println(name + " заселився у готель.");
+    }
+
+    @Override
+    public void checkOut() {
+        System.out.println(name + " виселився з готелю.");
+    }
 }
