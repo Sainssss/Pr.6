f450da41ad3481f00f2fee6ddc5942c87403574e
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:53:44 +0200

    Update CashPayment.java

--- src/hotel/CashPayment.java
+++ src/hotel/CashPayment.java
@@ -1,7 +1,22 @@
 package hotel;
 
 public class CashPayment extends Payment {
-    public CashPayment(String gName, double amt) { super(gName, amt); }
+    public CashPayment(String guestName, double amount) {
+        super(guestName, amount);
+    }
+
     @Override
-    public void process() { System.out.println(guestName + " оплатив " + amount + " грн готівкою"); }
+    public void process() {
+        System.out.println(guestName + " оплатив " + amount + " грн готівкою.");
+    }
+
+    @Override
+    public void refund() {
+        System.out.println("Повернення готівки для " + guestName);
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Готівковий платіж: " + amount + " грн");
+    }
 }
