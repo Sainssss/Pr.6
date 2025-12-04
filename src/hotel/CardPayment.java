cda9e70c1e66276387c2b0a792fac0d6f816f268
Sainssss <skalatskiandre228@gmail.com>
2025-12-04 03:53:31 +0200

    Update CardPayment.java

--- src/hotel/CardPayment.java
+++ src/hotel/CardPayment.java
@@ -1,14 +1,25 @@
 package hotel;
 
 public class CardPayment extends Payment {
-    String cardType;
+    private String cardType;
 
-    public CardPayment(String gName, double amt, String type) {
-        super(gName, amt); cardType = type;
+    public CardPayment(String guestName, double amount, String cardType) {
+        super(guestName, amount);
+        this.cardType = cardType;
     }
 
     @Override
     public void process() {
         System.out.println(guestName + " оплатив " + amount + " грн карткою (" + cardType + ")");
+    }
+
+    @Override
+    public void refund() {
+        System.out.println("Повернення коштів на карту для " + guestName);
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Картковий платіж: " + amount + " грн (" + cardType + ")");
     }
 }
